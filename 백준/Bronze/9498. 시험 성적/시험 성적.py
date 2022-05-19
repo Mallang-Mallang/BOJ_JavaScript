c30b228813140fbad80a3179f05d503ae725a9cd
Mallang-Mallang <70959328+Mallang-Mallang@users.noreply.github.com>
2022-05-20 01:32:34 +0900

    [Bronze IV] Title: 시험 성적, Time: 76 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+testScore = int(input())
+
+if 90 <= testScore:
+    print('A')
+elif 80 <= testScore:
+    print('B')
+elif 70 <= testScore:
+    print('C')
+elif 60 <= testScore:
+    print('D')
+else:
+    print('F')

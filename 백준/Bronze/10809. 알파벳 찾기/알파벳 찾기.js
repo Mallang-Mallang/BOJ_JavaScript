5ccefadd1cda6b3388ae87ed1cb4f1ea0a082410
Mallang-Mallang <70959328+Mallang-Mallang@users.noreply.github.com>
2022-05-23 13:47:50 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,20 @@
+//백준 입력값 복붙
+let userInput = `baekjoon`;
+// 백준서버에서 입력값 받아오는 부분    전처리 X
+let input =
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString().trim()
+    : userInput;
+
+let answer = "";
+
+  for (let j = 97; j <= 122; j++) {
+    if (input.includes(String.fromCharCode(j))) {
+      answer += input.indexOf(String.fromCharCode(j)) + " ";
+    }else {
+      answer += -1 + " ";
+    }
+  }
+
+
+console.log(answer);
